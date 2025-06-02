4b3aadff7a56bcd9077bccd47f8fa625e76eac48
petraalpigiano <petraalpigiano@gmail.com>
2025-06-02 12:59:08 +0200

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,38 @@
 // Il filtro deve funzionare dinamicamente quando l'utente seleziona un genere dalla select.
 // Se non viene selezionato alcun genere, devono essere mostrati tutti i film.
 import movies from "./data/movies";
+import { useState } from "react";
 
 export default function App() {
+  const [option, setOption] = useState("");
   return (
     <>
       <div className="container">
-        <select className="form-select" aria-label="Default select example">
-          <option>Filtra per genere</option>
-          <option>One</option>
-          <option>Two</option>
-          <option>Three</option>
+        <select
+          value={option}
+          onChange={function (event) {
+            setOption(event.target.value);
+          }}
+          className="form-select"
+          aria-label="Default select example"
+        >
+          <option value="">Filtra per genere</option>
+          {/* per iterare i generi userei un filter, perche non deve includere quelli doppi */}
+          {movies.map(function (currentMovie, index, array) {
+            return (
+              <option key={index} value={currentMovie.genre}>
+                {currentMovie.genre}
+              </option>
+            );
+            // FILTRO PER NON AVERE GENERI DUPLICATI
+            // return (
+            //  // !array.includes(currentMovie.genre) && (
+            //     <option key={index} value={currentMovie.genre}>
+            //       {currentMovie.genre}
+            //     </option>
+            //   )
+            // );
+          })}
         </select>
         {movies.map(function (currentMovie, index) {
           return (
